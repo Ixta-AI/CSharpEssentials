b130c7ae80c8818aebe7e46a76f9ed26b61dc03f
Adrian Ixta <1077056@uvu.edu>
2024-01-26 18:23:06 -0700

    Added myClass

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -7,3 +7,46 @@
 //Add another class with any name, add variables 
 //Using the new keyword create several objects
 //Console writeline the Objects variables
+using System;
+
+public class myClass
+{
+    public characterClass;
+        string characterClass = "Necromancer";
+    
+    public characterName;
+        string characterName = "Azucina";
+    
+    public characterHight;
+        public string mesurment = "ft";
+            int characterHight = 7;
+    
+    public characterType;
+        string characterType = "Type 2";
+    
+    public characterAge;
+        int characterAge = 22;
+}
+    public static void Main(string[] args)
+    {
+        characterClass = new character();
+        
+        characterClass.level = 6;
+        characterClass.health = 15;
+        characterClass.maxMana = 50;
+        characterClass.summonMaxCount = 4;
+
+        Console.writeline(characterName);
+        Console.WriteLine(characterClass);
+        Console.writeline(characterAge);
+        Console.writeline(characterType);
+        Console.writeline(characterHight);
+    }
+
+public class characterClass{
+    public int level;
+    public int health;
+    public int maxMana;
+    public int summonMaxCount;
+
+}
